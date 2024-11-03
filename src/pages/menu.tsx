3f46a4c07165b7034bef89edfc93790789e83f1e
root <root@DESKTOP-SL4V7UT>
2024-11-03 11:31:10 -0400

    tirei o estilo do body e coloquei em uma div

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -5,39 +5,41 @@ import '../img/fundoBotoes.png'
 const Menu = () => {
   return (
     <>
-      <header>
-        <a style={{ marginLeft: "15px", }}><b>Tel. (67) 3322 - 9911</b></a>
-        <a style={{ marginRight: "15px" }}>
-          <b>Suporte</b>
-        </a>
-      </header>
-      <main>
-        <div className="InformacoesDoUsuario">
-          <p style={{ marginLeft: "40px" }}><strong>Olá</strong></p>
-          <p style={{ marginRight: "40px", marginLeft: "auto" }}>Carlos Almeida</p>
-        </div>
-
-
-        <div className="CampoBotoes">
-
-          <div style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "space-evenly",
-            height: "40%",
-            marginTop: "80px",
-          }}>
-
-            <button type="button">Agenda</button>
-            <button type="button">Exames</button>
+      <div style={{backgroundColor: "#71cd97"}}>
+        <header>
+          <a style={{ marginLeft: "15px", }}><b>Tel. (67) 3322 - 9911</b></a>
+          <a style={{ marginRight: "15px" }}>
+            <b>Suporte</b>
+          </a>
+        </header>
+        <main>
+          <div className="InformacoesDoUsuario">
+            <p style={{ marginLeft: "40px" }}><strong>Olá</strong></p>
+            <p style={{ marginRight: "40px", marginLeft: "auto" }}>Carlos Almeida</p>
           </div>
 
-          <div style={{ display: "flex", width: "60%", justifyContent: "space-around", height: "50%" }}>
-            <button type="button">Dialise</button>
-          </div>
 
-        </div>
-      </main>
+          <div className="CampoBotoes">
+
+            <div style={{
+              display: "flex",
+              width: "100%",
+              justifyContent: "space-evenly",
+              height: "40%",
+              marginTop: "80px",
+            }}>
+
+              <button type="button">Agenda</button>
+              <button type="button">Exames</button>
+            </div>
+
+            <div style={{ display: "flex", width: "58%", justifyContent: "space-around", height: "50%" }}>
+              <button type="button">Dialise</button>
+            </div>
+
+          </div>
+        </main>
+      </div>
       <footer></footer>
     </>
   );
