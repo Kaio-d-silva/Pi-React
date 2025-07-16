893760159844096461da5a6fe0137f0678034ab2
Your Name <you@example.com>
2025-07-15 21:01:34 -0400

    collapse transitios

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -1,7 +1,6 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useRef, useState } from "react";
 
-import style from  '../assets/css/Collapse.module.css';
- 
+import styles from "../assets/css/Collapse.module.css";
 
 interface CollapseProps {
   title: string;
@@ -9,11 +8,29 @@ interface CollapseProps {
 }
 
 const Collapse: React.FC<CollapseProps> = ({ title, children }) => {
+    const [open, setOpen] = useState<boolean>(false);
+    const contentRef = useRef<HTMLDivElement>(null);
+  
+    const toggleCollapse = () => {
+      setOpen((prev) => !prev);
+    };
+    
   return (
-    <details className={style.collapse}>
-      <summary className={style.collapseTitle}>{title}</summary>
-      <div className={style.collapseContent}>{children}</div>
-    </details>
+    <div className={styles.collapse}>
+      <div className={styles.collapseTitle} onClick={toggleCollapse}>
+        {title}
+        <span>{open ? "▲" : "▼"}</span>
+      </div>
+      <div
+        ref={contentRef}
+        className={styles.collapseContent}
+        style={{
+          maxHeight: open ? `${contentRef.current?.scrollHeight}px` : "0px",
+        }}
+      >
+        {children}
+      </div>
+    </div>
   );
 };
 
